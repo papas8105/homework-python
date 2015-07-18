ab638b4ac7a3d581217378fa0406660903d54009
Georgios Papadopoulos <pgeorgios8@gmail.com>
2015-07-18 15:56:17 +0300

    Update homework-1.py
    Changed a few lines.

--- homework-1.py
+++ homework-1.py
@@ -1,4 +1,4 @@
-#This is a test for an absurd homeword
+#This is a test for an abstract homeword
 stud_id = input('Enter your name and surname seperated by a space (First Letters of name and surname in uppercase): ')
 w = []
 question1 = 'What is the sum of all the angles of a triange?'
@@ -21,13 +21,6 @@ def tobits(s):
         bits = '00000000'[len(bits):] + bits
         result.extend([int(b) for b in bits])
     return result
-''''
-def frombits(bits):
-    chars = []
-    for b in range(len(bits) / 8):
-        byte = bits[b*8:(b+1)*8]
-        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
-    return ''.join(chars)'''
-#continue in the algorithm
+#continue with the algorithm
 key = len(tobits(stud_id))*100
 print("Dear {} send the following characters at pgeorgios8@gmail.com in order for me to receive your grade: {}{}{}".format(stud_id,chr(key),chr(key+grade),chr(grade%key)))
